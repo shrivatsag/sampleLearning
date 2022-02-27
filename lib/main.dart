717bb55458bbc00d784f0ad44dc9040f65c5c4d5
shrivatsa G <shrivatsaganapathi@gmail.com>
2022-02-27 22:43:08 +0530

    new commit 10.45

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,15 @@ class Test extends StatelessWidget {
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 113, 158, 29),
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('assets/city1.jpg'),
-          ),
+        body: Center(
+          //Elevated button is a new development, can use this code
+          child: ElevatedButton(
+              //the below snippet is used to add style separately
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              onPressed: () {},
+              child: const Text('click me')),
         ),
         floatingActionButton: FloatingActionButton(
           child: const Text('click'),
