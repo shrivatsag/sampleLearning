14a450a0cc2afeb1f67c34edbd04098060f00130
shrivatsa G <shrivatsaganapathi@gmail.com>
2022-02-27 22:07:48 +0530

    commited

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('movio'),
-            centerTitle: true,
-            backgroundColor: Color.fromARGB(255, 113, 158, 29),
-          ),
-          body: Center(
-            child: Text(
-              'hello ninjas',
-              //to style the text use style: TextStyle
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-                color: Color.fromARGB(255, 18, 75, 122),
-                fontFamily: 'Quintessential',
-              ),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Text('click'),
-            onPressed: () {},
-            backgroundColor: Colors.red,
-          ),
-        ),
-      ),
-    );
+void main() => runApp(const Test());
 
-class test extends StatelessWidget {
-  const test({Key? key}) : super(key: key);
+class Test extends StatelessWidget {
+  const Test({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('movio'),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 113, 158, 29),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('assets/city1.jpg'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Text('click'),
+          onPressed: () {},
+          backgroundColor: Colors.red,
+        ),
+      ),
+    );
   }
 }
